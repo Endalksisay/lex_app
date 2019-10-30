47ee9b23af8d2166131fa5371983b3ec6fc472f7
Nikita Voloshenko <stivens54@gmail.com>
2019-10-29 18:15:31 -0700

    added SSO

--- app/src/main/java/blockchain/example/lex/MainActivity.java
+++ app/src/main/java/blockchain/example/lex/MainActivity.java
@@ -3,15 +3,31 @@ package blockchain.example.lex;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.text.TextUtils;
+import android.util.Log;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -22,68 +38,255 @@ import blockchain.example.lex.Model.User;
 import blockchain.example.lex.Prevalent.Prevalent;
 import io.paperdb.Paper;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
 
 
-    private Button
-    joinNowButton, loginButton;
+    // Google Sign In
+    private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private TextView mStatusTextView;
+    private TextView mDetailTextView;
+
+
+
+    private Button joinNowButton, loginButton;
     private ProgressDialog loadingBar;
 
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        joinNowButton = findViewById(R.id.main_join_now_btn);
-        loginButton = findViewById(R.id.main_login_btn);
-        loadingBar = new ProgressDialog(this);
-
-
-
-        Paper.init(this);
-
-        loginButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-                    public void onClick(View view)
-        {
-           Intent intent = new Intent (MainActivity.this, LoginActivity.class);
-           startActivity(intent);
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.signInButton) {
+            signIn();
         }
-    });
+    }
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
 
+        mProgressDialog.show();
+    }
 
-      joinNowButton.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view)
-          {
-              Intent intent = new Intent (MainActivity.this, RegisterActivity.class);
-              startActivity(intent);
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
 
-          }
-      });
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
-   String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
-   String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey) ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
-      if(!UserPhoneKey.equals("") && !UserPasswordKey.equals(""))
-        {
-            if (!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey))
-            {
-                AllowAccess(UserPhoneKey, UserPasswordKey);
+        // Views
+//        mStatusTextView = findViewById(R.id.status);
+//        mDetailTextView = findViewById(R.id.detail);
 
+        // Button listeners
+        findViewById(R.id.signInButton).setOnClickListener(this);
+//        findViewById(R.id.signOutButton).setOnClickListener(this);
+//        findViewById(R.id.disconnectButton).setOnClickListener(this);
 
-                loadingBar.setTitle("Already Logged in");
-                loadingBar.setMessage("Please wait.....");
-                loadingBar.setCanceledOnTouchOutside(false);
-                loadingBar.show();
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+
+            findViewById(R.id.signInButton).setVisibility(View.GONE);
+//            findViewById(R.id.signOutAndDisconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+            mDetailTextView.setText(null);
+
+            findViewById(R.id.signInButton).setVisibility(View.VISIBLE);
+//            findViewById(R.id.signOutAndDisconnect).setVisibility(View.GONE);
+        }
+    }
+
+//    @Override
+//    public void onClick(View v) {
+//        int i = v.getId();
+//        if (i == R.id.signInButton) {
+//            signIn();
+//        } else if (i == R.id.signOutButton) {
+//            signOut();
+//        } else if (i == R.id.disconnectButton) {
+//            revokeAccess();
+//        }
+//    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // ...
             }
         }
-
     }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        // [START_EXCLUDE silent]
+        showProgressDialog();
+        // [END_EXCLUDE]
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.drawer_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+
+
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        joinNowButton = findViewById(R.id.main_join_now_btn);
+//        loginButton = findViewById(R.id.main_login_btn);
+//        loadingBar = new ProgressDialog(this);
+//
+//
+//
+//        Paper.init(this);
+//
+//        loginButton.setOnClickListener(new View.OnClickListener()
+//        {
+//            @Override
+//                    public void onClick(View view)
+//        {
+//           Intent intent = new Intent (MainActivity.this, LoginActivity.class);
+//           startActivity(intent);
+//        }
+//    });
+//
+//
+//      joinNowButton.setOnClickListener(new View.OnClickListener() {
+//          @Override
+//          public void onClick(View view)
+//          {
+//              Intent intent = new Intent (MainActivity.this, RegisterActivity.class);
+//              startActivity(intent);
+//
+//          }
+//      });
+//
+//   String UserPhoneKey = Paper.book().read(Prevalent.UserPhoneKey);
+//   String UserPasswordKey = Paper.book().read(Prevalent.UserPasswordKey) ;
+//
+//      if(!UserPhoneKey.equals("") && !UserPasswordKey.equals(""))
+//        {
+//            if (!TextUtils.isEmpty(UserPhoneKey) && !TextUtils.isEmpty(UserPasswordKey))
+//            {
+//                AllowAccess(UserPhoneKey, UserPasswordKey);
+//
+//
+//                loadingBar.setTitle("Already Logged in");
+//                loadingBar.setMessage("Please wait.....");
+//                loadingBar.setCanceledOnTouchOutside(false);
+//                loadingBar.show();
+//
+//            }
+//        }
+//
+//    }
 
     private void AllowAccess(final String phone, final String password)
     {
