3aaa937f8561e38b122b52344644b6810fc1623b
Skrumpcious <iamfes007@aol.com>
2019-11-12 21:59:35 -0800

    Added company to token

--- app/src/main/java/blockchain/example/lex/Model/Token.java
+++ app/src/main/java/blockchain/example/lex/Model/Token.java
@@ -10,6 +10,7 @@ public class Token {
     private String publicKey;
     private String tokenName;
     private String description;
+    private String companyName;
     private String logoLocation;
     private ArrayList<Transaction> tokenTransactions;
 
@@ -21,6 +22,7 @@ public class Token {
         publicKey = "";
         tokenName = "";
         description = "";
+        companyName = "";
         logoLocation = "";
         tokenTransactions = new ArrayList<>();
 
@@ -73,6 +75,33 @@ public class Token {
         this.tokenTransactions = (ArrayList) tokenTransactions.clone();
     }
 
+    /**
+     * Constructor that takes all String arguments and an ArrayList arguments
+     * for the transactions.
+     * @param publicKey Public key for the token.
+     * @param tokenName Name of the token.
+     * @param description Description of this token.
+     * @param companyName Name of company associated with token.
+     * @param logoLocation File location of the logo.
+     * @param tokenTransactions List of transactions for this token.
+     * Precondition: All String arguments must be Strings and not null and
+     * ArrayList must be a valid ArrayList.
+     * Postcondition: Token object will be created.
+     */
+    public Token(String publicKey, String tokenName, String description,
+                 String companyName, String logoLocation, ArrayList<Transaction> tokenTransactions)
+    {
+        if (publicKey != null && tokenName != null && description != null && companyName != null)
+        {
+            this.publicKey = publicKey;
+            this.tokenName = tokenName;
+            this.description = description;
+            this.companyName = companyName;
+        }
+        tokenTransactions = new ArrayList<>();
+        this.tokenTransactions = (ArrayList) tokenTransactions.clone();
+    }
+
     //Accessors
 
     /**
@@ -90,6 +119,12 @@ public class Token {
      * @return Token's description
      */
     public String getDescription(){return description;}
+
+    /**
+     * Accessor method that gets the name of the company associated with the token.
+     * @return Company's name
+     */
+    public String getCompanyName(){return companyName;}
     /**
      * Accessor method that gives access to the location of the token's logo.
      * @return Token's logo location.
@@ -118,6 +153,12 @@ public class Token {
     {
         if (description != null)
             this.description = description;
+    }
+
+    public void setCompanyName(String companyName)
+    {
+        if(companyName != null)
+            this.companyName = companyName;
     }
 
     public void setLogoLocation(String logoLocation)
