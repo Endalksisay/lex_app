3d38e11372328e6f501afd97e5429357d97afd2a
Alejandro Lopez <lopez.aj1516@gmail.com>
2019-11-17 21:52:56 -0800

    Added buttons to home page, they just need to be updated per token and they need new action listeners.

--- app/src/main/java/blockchain/example/lex/ui/home/LexFragment.java
+++ app/src/main/java/blockchain/example/lex/ui/home/LexFragment.java
@@ -1,12 +1,16 @@
 package blockchain.example.lex.ui.home;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import blockchain.example.lex.R;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class LexFragment extends Fragment {
     private double currentCurrency = 0;
@@ -49,25 +54,116 @@ public class LexFragment extends Fragment {
         String btnSetText = String.format(Locale.US, "$%.2f", currentCurrency);
         currencyButton.setText(btnSetText);
 
+//        LayoutInflater linf;
+//        LinearLayout rr;
+//
+//        linf = (LayoutInflater) getActivity().getApplicationContext().getSystemService(
+//                Context.LAYOUT_INFLATER_SERVICE);
+//        linf = LayoutInflater.from(this.getActivity());
+//
+//        rr = (LinearLayout) root.findViewById(R.id.tokenLayout);
+
+//        for (int i = 1; i < 5; i++) {
+//
+//            View v = linf.inflate(R.layout.fragment_lex, null);
+//            //v.setId(i);
+//            TextView text = v.findViewById(R.id.tokenDesc);
+//            text.setId(i);
+//            text.setText("Company name");
+//            text.setPadding(5,500,5,5);
+//
+//            rr.addView(v);
+//
+//        }
+
+//        for (int i = 1; i <= 5; i++)
+//        {
+//            LinearLayout linear = root.findViewById(R.id.companyButtonLayout);
+//
+//        }
+
         for (int i = 1; i <= 20; i++) {
             LinearLayout linear = root.findViewById(R.id.companyButtonLayout);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             Button btn = new Button(root.getContext());
+            LinearLayout layout2 = new LinearLayout(root.getContext());
+//            layout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout2.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            layout2.setOrientation(LinearLayout.HORIZONTAL);
+            layout2.setBackgroundColor(Color.WHITE);
+
+            int left = 10;
+            int top = 15;
+            int right = 10;
+            int bottom = 15;
+
+            TableRow.LayoutParams parameters = new TableRow.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            parameters.setMargins(left, top, right, bottom);
+
+            //EditText edXY = new EditText(inventory.this);
+            layout2.setLayoutParams(parameters);
+
+//            android:layout_width="96dp"-->
+//<!--                    android:layout_height="106dp"-->
+
+            layout2.setId(i);
+//            layout2.lay
             btn.setId(i);
+            CircleImageView image = new CircleImageView(root.getContext());
+            image.setId(i);
+            ;
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(150, 150);
+//            image.requestLayout();
+            image.setLayoutParams(layoutParams);
+            image.setPadding(10,10,10,10);
+//            image.getLayoutParams().width = 96;
+//            image.getLayoutParams().height = 106;
+            image.setImageResource(R.drawable.alejandro);
+//            image.setMaxWidth(96);
+//            image.setMaxHeight(106);
+
+
+
+//            image.setImageURI("../../../../res/drawable/alejandro.png");
+            layout2.addView(image);
+            linear.addView(layout2);
+
+            TextView tv1 = new TextView(root.getContext());
+            TextView tv2 = new TextView(root.getContext());
+
+            tv1.setTextSize(25);
+            tv2.setTextSize(25);
+
+            tv1.setText("Company " + i);
+            tv2.setText(i + "00.00");
+
+            tv1.setGravity(Gravity.CENTER);
+            tv2.setGravity(Gravity.RIGHT);
+            tv1.setPadding(200,10,1,10);
+            tv2.setPadding(150,10,1,10);
+            //tv2.layout(10,10,10,10);
+
+            layout2.addView(tv1);
+            layout2.addView(tv2);
+//            lBut.setId(i);
             final int id_ = btn.getId();
+//            final int lBId_ = lBut.getId();
             String btnText = "button " + id_;
+//            String lButText = "button " + lBId_;
             btn.setText(btnText);
-            linear.addView(btn, params);
-            btn1 = (root.findViewById(id_));
-            btn1.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Toast.makeText(view.getContext(),
-                            "Button clicked index = " + id_, Toast.LENGTH_SHORT)
-                            .show();
-                }
-            });
+
+//            linear.addView(btn, params);
+//            linear.addView(lBut, params);
+//            btn1 = (root.findViewById(id_));
+//            btn1.setOnClickListener(new View.OnClickListener() {
+//                public void onClick(View view) {
+//                    Toast.makeText(view.getContext(),
+//                            "Button clicked index = " + id_, Toast.LENGTH_SHORT)
+//                            .show();
+//                }
+//            });
         }
 
         return root;
